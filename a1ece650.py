20dcdb6873430874d48a7f8ddf9495bcf0433fba
Michael Lynch <mikrlynch@gmail.com>
2019-09-19 18:25:26 -0400

    Fleshed out parser function and added error codes

--- a1ece650.py
+++ a1ece650.py
@@ -1,7 +1,7 @@
 import sys
 import re
 
-# YOUR CODE GOES HERE
+
 
 def AddStreet(street_name, vertices):
     pass
@@ -23,13 +23,22 @@ def ParseInput(line):
     ie whitespace between each street arg and vertices, whitespace in vertices args don't matter
     note: this also matches the case where only one arg is passed in order to capture the case r
     """
-    command_parser = re.compile("(^[ ]*(a|c|g|r)[ ]*)")
-    arg_parser = re.compile("([ ]+\"[a-z]+\"[ ]+(\([ ]*[0-9]+[ ]*[\,][ ]*[0-9]+[ ]*\)[ ]*)+)|([ ]+\"[a-z]+\"$)")
-
+    command_parser = re.compile(r"(^[ ]*(a|c|g|r)[ ]*)")
+    arg_parser = re.compile(r"([ ]+\"[a-z]+\"[ ]+(\([ ]*[0-9]+[ ]*[\,][ ]*[0-9]+[ ]*\)[ ]*)+)|([ ]+\"[a-z]+\"$)")
 
     
-    # placeholder return statement
-    return "a", "args", "more_args"
+    if command_parser.search(line) is not None: # If there is a match, save into command variable
+        command = command_parser.match(line).group()
+    else:
+        return None, 41 # Else return nothing for the command and the error code
+    
+    assert(command in ['a', 'c', 'r', 'g'])
+
+    
+    command_sequence = [command, "weber", [(1,2), (3,4)]]
+
+    #return the command sequence [command, street, [vertices] ] and a success code
+    return command_sequence, 0
 
 def main():
     ### YOUR MAIN CODE GOES HERE
@@ -38,26 +47,53 @@ def main():
     ### make sure to remove all spurious print statements as required
     ### by the assignment
 
+
     # Dict to hold references to functions
-    menu_options = {"a": AddStreet,
-                    "c": ChangeStreet,
-                    "r": RemoveStreet,
-                    "g": OutputGraph}
+    menu_options = {'a': AddStreet,
+                    'c': ChangeStreet,
+                    'r': RemoveStreet,
+                    'g': OutputGraph}
     
-    
+    # Dict to hold possible errors
+    global error_codes
+    error_codes = {01:"Error: Ruh roh!",
+                   02:"Error: Invalid command",
+                   41:"Error: Command not found"} 
+        
+    # wait for input
     while True:
+
+        # Read a line of input
         line = sys.stdin.readline().strip()
 
+        # If line is blank, exit program
         if line == '':
             break
 
-        command, street_name, vertices = ParseInput(line)
+        # Else parse the input line
+        command_sequence, return_value = ParseInput(line)
 
-        try:
-            menu_options[command](street_name, vertices)
-        except:
+        # Figure out if an error occurred within the function
+        # If so, skip back to input
+        if return_value is not 0:
+            sys.stderr.write(error_codes[return_value]+'\n')
+            continue
+        
+        if command_sequence[0] in ['a', 'c']:
+            pass
+        elif command_sequence[0] is 'r':
+            pass
+        elif command_sequence[0] is 'g':
             pass
         
+        else:
+            sys.stderr.write("Error: "+error_codes[02])
+            
+        street_name="Placeholder1"
+        vertices = "Placeholder2"
+        command=command_sequence[0]
+        menu_options[command](street_name, vertices)
+    
     print '[*]Exiting graph generator'
     # return exit code 0 on successful termination
     sys.exit(0)
