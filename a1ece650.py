53e07a05e11df83ec48003e2a83f2e1205cb10c1
Michael Lynch <mikrlynch@gmail.com>
2019-09-19 15:37:19 -0400

    Fixed input loop and added skeleton of program

--- a1ece650.py
+++ a1ece650.py
@@ -2,14 +2,35 @@ import sys
 
 # YOUR CODE GOES HERE
 
+def AddStreet(street_name, vertices):
+    pass
+
+def ChangeStreet(street_name, vertices):
+    pass
+
+def RemoveStreet(street_name):
+    pass
+
+def OutputGraph():
+    pass
+
+
 def main():
     ### YOUR MAIN CODE GOES HERE
 
     ### sample code to read from stdin.
     ### make sure to remove all spurious print statements as required
     ### by the assignment
+
+    # Dict to hold references to functions
+    menu_options = {"a": AddStreet,
+                    "c": ChangeStreet,
+                    "r": RemoveStreet,
+                    "g": OutputGraph}
+    
+    
     while True:
-        line = sys.stdin.readline()
+        line = sys.stdin.readline().strip()
         if line == '':
             break
         print 'read a line:', line
