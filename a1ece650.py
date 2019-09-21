285e1eda2515ff401d4346db7c8db7a17dd6473c
Michael Lynch <mikrlynch@gmail.com>
2019-09-21 01:49:08 -0400

    Made parser work

--- a1ece650.py
+++ a1ece650.py
@@ -2,8 +2,6 @@ import sys
 import re
 import pdb #debug
 
-
-
 def AddStreet(street_name, vertices):
     print "Add street function"
 
@@ -18,7 +16,7 @@ def OutputGraph():
 
 def errorHandler(command, arguments, street_name, vertices):
 
-    error_code = 0 # default success
+    error_code = 500 # uncaught error. bad
 
     if (command is 'a' or command is 'c'): #input errors for a|c
         if not street_name and not vertices: #if we don't find a street name or vertex set
@@ -55,42 +53,61 @@ def ParseInput(line):
 
     vertex_parser parses the vertices
     """
+
+    add_change_parser = re.compile(r"(([ ]*(a|c)[ ]+)(\"[a-z A-Z]+\"[ ]+)((\([ ]*[0-9]+[ ]*\,[ ]*[0-9]+[ ]*\)[ ]*)+))")
+    remove_parser = re.compile(r"^([ ]*r)([ ]+\"[a-z A-Z]+\")$")
+    graph_parser = re.compile(r"^[ ]*g[ ]*$")
+    
     command_parser = re.compile(r"(^[ ]*(a|c|g|r)[ ]*)")
     arg_parser = re.compile(r"([ ]+[\"a-zA-Z]+[ ]*[a-zA-Z]*\"[ ]+(\([ ]*[0-9]+[ ]*[\,][ ]*[0-9]+[ ]*\)[ ]*)+)|([ ]+\"[a-zA-Z]+\"$)")
-    street_name_parser = re.compile(r"\"[a-zA-Z]+[ ]*[a-zA-Z]*\"")
+    street_name_parser = re.compile(r"([ ]*\")([a-z A-Z]+)(\"[ ]*)")
     vertex_parser = re.compile(r"\([ ]*[0-9]+[ ]*\,[ ]*[0-9]+[ ]*\)")
 
     parsed_output = {'output':[[],[],[]], 'error':0} #initialize return dict with empty command and OK code
     command = street_name = vertices = None #init our command structure  
-
-    """LOOK AT TEST CASE WHERE INPUT IS a"blabla"  """
-    if command_parser.search(line) is not None: # If there is a match, save into command variable
-        command = command_parser.match(line).group().strip() #grab the parsed command and remove any whitespace
-        #pdb.set_trace()
-        arguments = arg_parser.findall(line)
-        #check case : a "ababab" 
-        street_name = street_name_parser.findall(''.join(arguments)) #split args into street name
-        vertices = vertex_parser.findall(''.join(arguments)) #and vertices
-        #pdb.set_trace()
-
-        # handle the erroneous cases by setting an error code using our handler function
-        parsed_output['error'] = errorHandler(command, arguments, street_name, vertices)
-    
-        if parsed_output['error']==0: #if we got here without any problems
-            #process street name
-            #process vertices
-            command_sequence = [command, street_name, vertices] #build command sequence
-            parsed_output['output'] = command_sequence    #put command sequence in output field
-
-        else:
-            pass #parsed output will be in the form {'output':[], 'error':x} anyway
+    #pdb.set_trace()
+    if (add_change_parser.match(line) or remove_parser.match(line) or graph_parser.match(line)): # If there is a match, save into command variable
+        if add_change_parser.match(line):
+            command = add_change_parser.match(line).group(2).strip() #grab the parsed command and remove any whitespace
+            street_name = street_name_parser.match(add_change_parser.match(line).group(4)).group(2)
+            vertices = add_change_parser.match(line).group(5)
             
+        elif remove_parser.match(line):
+            command = remove_parser.match(line).group(1)
+            street_name = street_name_parser.match(remove_parser.match(line).group(2)).group(2)
+            
+        elif graph_parser.match(line):
+            command = graph_parser.match(line).group(0).strip()
         
-        
+            
+        #pdb.set_trace()
+
+
     else:
-        parsed_output['error'] = 510 # If no match, set error code 510
+        try:
+            if command_parser.match(line) is not None:
+                #pdb.set_trace()
+                command = command_parser.match(line).group().strip()
+                arguments = arg_parser.findall(line)
+                street_name = re.findall(r'\"[a-z A-Z]+\"', ''.join(arguments)) #split args into street name
+                vertices = vertex_parser.findall(''.join(arguments)) #and vertices
+                # handle the erroneous cases by setting an error code using our handler function
+                parsed_output['error'] = errorHandler(command, arguments, street_name, vertices)
+
+            else:
+                parsed_output['error'] = 510 # If no match, set error code 510 for erroneous input       
+        except:
+            parsed_output['error'] = 500 # unhandled error
+            
+    if parsed_output['error']==0: #if we got here without any problems
+        #prepare street name and vertices for return
+        command_sequence = [command, street_name, vertices] #build command sequence
+        parsed_output['output'] = command_sequence    #put command sequence in output field
+ 
+ 
         
     #return the command sequence (set or not) and a success/error code
+    #pdb.set_trace()
     return parsed_output
 
 def main():
@@ -111,11 +128,15 @@ def main():
     global error_codes
     error_codes = {01:"Error: Ruh roh!",
                    02:"Error: Invalid command",
+                   500:"Error: Exception raised",
                    510:"Error: Command not found",
-                   521:"Error: User chose a or c with incorrect input format. Please enter (a|c) \"<streetname\" (x1,y1) (x2,y2) (xi,yi)",
-                   522:"",
-                   523:"",
-                   524:""}
+                   511:"Error: Too many street names for this argument",
+                   521:"Error: Incorrect input format. (a|c) \"<street name>\" (x1,y1) (x2,y2) (xi,yi)",
+                   522:"Error: Please enter a street name for (a|c)",
+                   523:"Error: Please enter vertices for (a|c)",
+                   531:"Error: No street name for (r). (r) \"<street name>\"",
+                   532:"Error: Unnecessary vertices for (r). (r) \"<street name>\"",
+                   540:"Error: Too many arguments for g. (g)"}
                    
 
     # wait for input
@@ -141,22 +162,17 @@ def main():
         if return_value is not 0:
             sys.stderr.write(error_codes[return_value]+'\n')
             continue
-        
+
         if command in ['a', 'c']:
             menu_options[command](street_name, vertices)
-        elif command_sequence is 'r':
+        elif command is 'r':
             menu_options[command](street_name)
-        elif command_sequence is 'g':
+        elif command is 'g':
             menu_options[command]()
-        
-        else:
-            sys.stderr.write("Error: "+error_codes[02])
-            
-        street_name="Placeholder1"
-        vertices = "Placeholder2"
-        command=command_sequence[0]
-        menu_options[command](street_name, vertices)
     
+        else:
+            sys.stderr.write(error_codes[02]+'\n')
+        
     print '[*]Exiting graph generator'
     # return exit code 0 on successful termination
     sys.exit(0)
---
+++ test/test_cases.py
@@ -0,0 +1,5 @@
+for command in ['a', 'c', 'r', 'g']:
+    for street in ['', '"weber"', ' "weber"', '"weber" ', ' "weber" ']:
+       for vertex in ['','(1,2),(1,3)']:
+           command
+           this_test = command+street+vertex
